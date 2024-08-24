3b34ede7bd6446ef80d347ceba240171b1bb9bb3
bcharrey <61541836+bcharrey@users.noreply.github.com>
2024-08-24 12:47:45 +0200

    Created GameManager that contains playerName for now

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 [DefaultExecutionOrder(1000)]
 public class MenuUIHandler : MonoBehaviour
 {
+    public TMP_InputField tmpInputField;
+
     public void StartNew()
     {
+        GameManager.Instance.playerName = tmpInputField.text;
+
         SceneManager.LoadScene(1);
     }
 }
